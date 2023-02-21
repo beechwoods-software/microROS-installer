c1adca7369588c0dcdba86680e40e07b48d262b5
Brad Kemp <brad@beechwoods.com>
2023-02-21 11:06:54 -0500

    Genesis

---
+++ install_microros.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -z ${ROS_DISTRO} ]; then
+  echo ROS_DISTRO not set
+  exit -1
+fi
+board=esp32
+if [ -n $1 ]; then
+board=$1
+fi
+tmpname=`readlink -f  $0`
+prgdir=`dirname $tmpname`
+
+# Source the ROS 2 installation
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# Create a workspace and download the micro-ROS tools
+mkdir microros_ws
+cd microros_ws
+git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+pushd src/micro_ros_setup
+for x in $prgdir/00* ; do
+    patch -p 1 < $x
+done
+popd
+
+# Update dependencies using rosdep
+sudo apt update && rosdep update
+rosdep install --from-paths src --ignore-src -y
+
+# Install pip
+sudo apt-get install python3-pip
+
+# Build micro-ROS tools and source them
+colcon build
+source install/local_setup.bash
+
+ros2 run micro_ros_setup create_firmware_ws.sh zephyr $board 2>&1 | tee /tmp/microros-zephyr.log
+
