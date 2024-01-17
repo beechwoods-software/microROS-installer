b3341b94b9b6fe92910aecdd743db642b5c45c36
Dave Rensberger <davidr@beechwoods.com>
2024-01-17 11:40:00 -0500

    Removing Pico W specific stuff

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ unset AMENT_PREFIX_PATH
 
 export ROS_DISTRO=humble
 
+board=esp32
 #board=esp32_devkitc_wroom
-board=rpi_pico_w
-app=weatherstation
+#board=rpi_pico_w
+
+app=soilsensor
+#app=weatherstation
 
 repo=ssh://git@lm-gitlab.beechwoods.com:7999
-agent_ip=192.168.2.2
 #repo=ssh@192.168.111.100:/git
 
 while getopts "a:b:r:gh" OPTION; do
@@ -57,12 +59,11 @@ popd
 pushd microros_ws/firmware/mcu_ws
   git clone -b main $repo/micro-ros/idl/weatherstation
 popd
-
 pushd microros_ws
   source /opt/ros/$ROS_DISTRO/setup.bash
   source install/local_setup.bash
 
-  ros2 run micro_ros_setup configure_firmware.sh $app --transport udp -i $agent_ip -p 8888
+  ros2 run micro_ros_setup configure_firmware.sh $app --transport udp
   ros2 run micro_ros_setup build_firmware.sh  2>&1 | tee /tmp/microros_build_zephyr_${board}.log
   ros2 run micro_ros_setup create_agent_ws.sh
   ros2 run micro_ros_setup build_agent.sh --cmake-args -Wno-dev
